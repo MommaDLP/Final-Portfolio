d8a1fca152d2a1075e90070621b1de7bbbecc802
MommaDLP <159870485+MommaDLP@users.noreply.github.com>
2024-09-12 15:26:33 -0500

    About me

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+  return (
+    <>
+      <p>Hello World! I'm Miranda De La Paz a beginner web developer out of Texas.<br></br>
+         As an artist, my first love will always be crafting the visual.<br></br>
+         From creating an amazing home life with a family to lines of code on a webpage, <br></br>
+         I love combining logic with creative design to make eye catching websites and applications.<br></br>
+        <br></br>
+         I've just completed UTSA bootcamp and was able to leverage my creative skills with incredible web development tools.<br></br>
+         I've always been drawn to both the creative and analytical side of development, <br></br>
+         so I'm so thankful to be able to do both with the foundation I built during bootcamp.<br></br>
+         I’m now able to truly call myself a Front-End Developer who is currently working to becoming stronger in Back-End developing. <br></br>
+         With that technology leads, society follows and I'm excited to make the leap to continue to refine my skills with the right company.<br></br>
+      </p>
+    </>
+  )
+}
+
+export default App
