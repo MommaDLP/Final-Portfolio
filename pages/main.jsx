8185f107d1229c48287498ff44915085edac2595
MommaDLP <159870485+MommaDLP@users.noreply.github.com>
2024-09-20 23:00:47 -0500

    deleted and added to footer,nav,pages

---
+++ pages/main.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; //tailwindcss
+import App from './App';
+
+
+//routes to pages folder
+import App from './App';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Contact from "./pages/Contact/Contact";
+import Resume from "./pages/Resume/Resume";
+
+//const paths
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+      <div id='footer'>
+        <div className="container">
+  <footer id='connect' className="py-3">
+    <h3>Let's Connect</h3>
+    <a href='www.linkedin.com/in/miranda-de-la-paz-690898130'>LinkedIn</a>
+    <a href='https://github.com/MommaDLP'>GitHub</a>
+  </footer>
+</div>
+      </div>
+    )
+  }
+  
+  export default Footer
